9f211cdbab6276b97ad9997353ffbce2a21e113f
volsoft <sandro.lomazzo@hotmail.it>
2019-01-09 18:19:41 +0100

    comp

---
+++ WishList/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authorization;
+using WishList.Models;
+namespace WishList.Controllers
+{
+    public class AccountController : Controller
+    {
+
+
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        private readonly SignInManager<ApplicationUser> _sigInManager;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> sigInManager)
+        {
+            _userManager = userManager;
+            _sigInManager = sigInManager;
+        }
+
+
+    }
+}
