800d2b71f0334cb573feefd2fccd4d77fb4e58fa
volsoft <sandro.lomazzo@hotmail.it>
2019-01-09 18:46:19 +0100

    f

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Authorization;
 using WishList.Models;
+using WishList.Models.AccountViewModels;
+
 namespace WishList.Controllers
 {
     [Authorize]
@@ -23,6 +25,34 @@ namespace WishList.Controllers
             _signInManager = sigInManager;
         }
 
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            return View("Register");
+        }
 
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var ret = _userManager.CreateAsync(new ApplicationUser { Email = model.Email, UserName = model.Email }, model.Password).Result;
+
+            if (!ret.Succeeded)
+            {
+                foreach (var item in ret.Errors)
+                {
+                    ModelState.AddModelError("Password", item.Description);
+
+                    return View(model);
+                }
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
